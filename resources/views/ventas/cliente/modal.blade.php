62d552462ebb1b8f30b47d8bb0bbedf5ee7c042d
luisenrique <lorozco001@gmail.com>
2020-06-17 18:05:25 -0500

    creacion del modulo cliente

--- resources/views/ventas/cliente/modal.blade.php
+++ resources/views/ventas/cliente/modal.blade.php
@@ -1,7 +1,7 @@
 
 <div class="modal fade modal-slide-in-right" aria-hidden="true" 
-role="dialog" tabindex="-1" id="modal-delete-{{$cat->idcategoria}}">
-	{{Form::Open(array('action'=>array('CategoriaController@destroy',$cat->idcategoria),'method'=>'delete'))}}
+role="dialog" tabindex="-1" id="modal-delete-{{$per->idpersona}}">
+	{{Form::Open(array('action'=>array('ClienteController@destroy',$per->idpersona),'method'=>'delete'))}}
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
@@ -9,10 +9,10 @@ role="dialog" tabindex="-1" id="modal-delete-{{$cat->idcategoria}}">
 					aria-label="Close">
 	                     <span aria-hidden="true">×</span>
 	                </button>
-	                <h4 class="modal-title">Eliminar Categoría</h4>
+	                <h4 class="modal-title">Eliminar Cliente</h4>
 				</div>
 				<div class="modal-body">
-					<p>Confirme si desea Eliminar la Categoría</p>
+					<p>Confirme si desea Eliminar el Cliente</p>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
